e29aa169b514a140eb6fd27efe538d2316f600be
ShrutiSharma77 <shruti.sharma.met21@itbhu.ac.in>
2023-08-13 12:21:22 +0530

    email router

--- routers/mail.router.js
+++ routers/mail.router.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 var index = require('../controllers/mail.controller.js');
 
-router.post('/send-mail', index.sendMail);
+router.post('send-mail',index.sendMail);
+router.post('/send', index.send);
 
 module.exports = router;
